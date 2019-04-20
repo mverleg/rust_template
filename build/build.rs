6de47d99c3225a39dc991abb800ebd779b1d570a
Mark <mverleg.noreply@gmail.com>
2019-04-20 20:10:49 +0200

    More config parsing

---
+++ build/build.rs
@@ -0,0 +1,7 @@
+use configure_me;
+
+fn main() {
+    // configure_me
+    configure_me::build_script_with_man("config_spec.toml").unwrap();
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,14 @@
 mod dependencies;
+
+use configure_me::include_config;
+
+const CRATE_NAME: &'static str = env!("CARGO_PKG_NAME");
+include_config!();
+
+pub fn main() {
+    // Read configuration from files and environment.
+    let (server_config, _remaining_args) = Config::including_optional_config_files(&[
+        "config/config.yaml",
+    ]).unwrap_or_exit();
+
+}
