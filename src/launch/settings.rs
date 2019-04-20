7a3dfdce7684532bcb18ddb1421a2b478f60eea3
Mark <mverleg.noreply@gmail.com>
2019-04-20 22:42:26 +0200

    Reading configuration

--- src/launch/settings.rs
+++ src/launch/settings.rs
@@ -1,10 +1,12 @@
 use std::convert::Into;
 use std::env;
 
-use config::{Config, ConfigError, Environment, File};
+use config::{Config, Environment, File};
 use serde::Deserialize;
 
 use super::CRATE_NAME;
+
+//TODO @mark: .env file
 
 #[derive(Debug, Deserialize)]
 pub struct Settings {
@@ -14,36 +16,45 @@ pub struct Settings {
 }
 
 impl Settings {
-    pub fn new() -> Result<Self, ConfigError> {
+    pub fn new() -> Self {
         let mut conf = Config::new();
 
         // TODO @mark: Files are read in this order:
-        // * user profile (e.g. inside ~/.config/)
+        // TODO * user profile (e.g. inside ~/.config/)
         // * base settings (config.yaml)
         // * [mode].yaml (from RUN_MODE, i.e. development.yaml)
         // * local.yaml (do not check this into VCS)
         // * environment
 
+        //TODO @mark: parse unwrap_or_else messages
+
         // Start with the "default" configuration file.
-        conf.merge(File::with_name("src/launch/config.defaults.yaml"))?;
+        conf.merge(File::with_name("src/launch/config.defaults.yaml"))
+            .unwrap_or_else(|err| panic!("Failed to parse default configuration: {:?}", err));
 
         // Add the base user config file.
-        conf.merge(File::with_name("config/config.yaml").required(false))?;
+        conf.merge(File::with_name("config/config.yaml").required(false))
+            .unwrap_or_else(|err| panic!("Failed to parse config.yaml (base file): {:?}", err));
 
         // Add in the current environment file ('development' by default)
         let mode = env::var("RUN_MODE").unwrap_or("development".into());
-        conf.merge(File::with_name(&format!("config/{}.yaml", mode)).required(false))?;
+        let mode_file_name = format!("config/{}.yaml", mode);
+        conf.merge(File::with_name(&mode_file_name).required(false))
+            .unwrap_or_else(|err| panic!("Failed to parse {} (based on RUN_MODE): {:?}", mode_file_name, err));
 
         // Add in a local configuration file
-        conf.merge(File::with_name("config/local.yaml").required(false))?;
+        conf.merge(File::with_name("config/local.yaml").required(false))
+            .unwrap_or_else(|err| panic!("Failed to parse local.yaml (override file): {:?}", err));
 
         // Add in settings from the environment (with a prefix)
-        conf.merge(Environment::with_prefix(CRATE_NAME))?;
+        conf.merge(Environment::with_prefix(CRATE_NAME))
+            .unwrap_or_else(|err| panic!("Failed to parse environment settings: {:?}", err));
 
         // You may also programmatically change settings
-        conf.set("run_mode", mode)?;
+        conf.set("run_mode", mode).unwrap();
 
         // You can deserialize (and thus freeze) the entire configuration as
         conf.try_into()
+            .unwrap_or_else(|err| panic!("Failed to read configuration: {:?}", err))
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,9 @@ use launch::settings::Settings;
 
 pub fn main() {
     // Read configuration from files and environment.
-    let settings = launch::settings::Settings::new();
+    let settings = Settings::new();
     //TODO @mark: log:
 //    info!("{:?}", settings);
+    println!("{:?}", settings);
 
 }
