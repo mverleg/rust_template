11d2d8fa81d85aecefc2339feea530910b8c3741
Mark <mverleg.noreply@gmail.com>
2019-12-26 18:51:29 +0100

    Report a git hash and unstaged files with the Rust binary

--- build/build.rs
+++ build/build.rs
@@ -1,6 +1,13 @@
-use configure_me_codegen::build_script_with_man;
+//use configure_me_codegen::build_script_with_man;
+mod code_version;
+use crate::code_version::extract_git_summary;
 
 fn main() {
     // configure_me
-    build_script_with_man("build/config_spec.toml").unwrap();
+//    build_script_with_man("build/config_spec.toml").unwrap();
+    println!("cargo:rerun-if-changed=build.rs");
+    let git_summary = extract_git_summary();
+    println!("cargo:rerun-if-changed=.git/index");
+    println!("cargo:rerun-if-changed=src");
+    println!("cargo:rustc-env=VCS_SUMMARY={}", git_summary);
 }
---
+++ build/code_version.rs
@@ -0,0 +1,37 @@
+use std::process::Command;
+
+pub fn extract_git_summary() -> String {
+    let hash = git_command_output(&["rev-parse", "HEAD"], "hash")
+        .trim().to_owned();
+    let files = git_command_output(&["status", "--porcelain"], "hash");
+    if files.is_empty() {
+        return format!("git+{}", hash)
+    }
+    let postfix = files.lines()
+        .map(|ln| ln.chars().skip(3).collect::<String>().trim().to_owned())
+        .collect::<Vec<_>>().join("+");
+    format!("git+{}+uncommitted_changed+{}", hash, postfix)
+}
+
+fn git_command_output(git_args: &[&str], git_description: &str) -> String {
+    let output = match Command::new("git").args(git_args).output() {
+        Ok(out) => {
+            if !out.status.success() || !out.stderr.is_empty() {
+                eprintln!("A problem occurred while getting git {}; is this a git repository that has commits?", git_description);
+                match String::from_utf8(out.stderr) {
+                    Ok(errmsg) => eprintln!("Code: {}, message: {}", out.status, errmsg),
+                    Err(_) => eprintln!("Code: {} (stderr contained non-UTF8 data)", out.status),
+                }
+                panic!();
+            }
+            out.stdout
+        },
+        Err(err) => {
+            eprintln!("The command to get git {} failed; is this a git repository that has commits?", git_description);
+            eprintln!("Error: {}", err);
+            panic!();
+        },
+    };
+    String::from_utf8(output)
+        .expect("git command output contained non-UTF8 data")
+}
