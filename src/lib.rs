c0ffee0c3601f49ce2bdd2af7a034177ca02cf3e
Mark <mverleg.noreply@gmail.com>
2021-12-25 18:30:31 +0100

    Create a template with a binary and library, using severan common dependencies

---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+use ::log::info;
+
+pub async fn run() {
+    info!("Hello, world!")
+}
---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use ::std::path::PathBuf;
+
+use ::derive_getters::Getters;
+use ::env_logger;
+use ::structopt::StructOpt;
+use ::tokio;
+
+use ::todotodo::run;
+
+#[cfg(feature = "jemalloc")]
+#[global_allocator]
+static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
+
+#[derive(Debug, StructOpt, Getters)]
+#[structopt(
+    name = "todo",
+    about = "todo"
+)]
+pub struct Args {
+    #[structopt(long = "files", short = "f", parse(from_os_str))]
+    files: Vec<PathBuf>,
+}
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+    let _args = Args::from_args();
+    run().await
+}
